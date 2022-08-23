6ec27dd04bce8d57437619ee4f460b3a591e5b35
Mark Alston <malston@vmware.com>
2022-08-23 19:42:59 +0000

    Create var for bosh vm

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,6 +9,11 @@ if [[ -z "${DEPLOYMENT}" ]]; then
     read -r DEPLOYMENT
 fi
 
+if [[ -z "${BOSH_VM_IP}" ]]; then
+    echo -n "Enter the IP for bosh director: "
+    read -r BOSH_VM_IP
+fi
+
 if [[ -z "${VCAP_PASSWORD}" ]]; then
     echo -n "Enter password for vcap: "
     read -rs VCAP_PASSWORD
@@ -21,7 +26,7 @@ cat > "$script_dir/pdm.sh" <<EOF
 /home/vcap/bin/pdm -n $DEPLOYMENT
 EOF
 
-if sshpass -p $VCAP_PASSWORD ssh vcap@10.176.168.22 -q "bash -s " <  "$__DIR/install.sh" > /dev/null 2>&1; then
-    sshpass -p $VCAP_PASSWORD ssh vcap@10.176.168.22 -q "bash -s " <  "$script_dir/pdm.sh"
+if sshpass -p $VCAP_PASSWORD ssh "vcap@$BOSH_VM_IP" -q "bash -s " <  "$__DIR/install.sh" > /dev/null 2>&1; then
+    sshpass -p $VCAP_PASSWORD ssh "vcap@$BOSH_VM_IP" -q "bash -s " <  "$script_dir/pdm.sh"
 fi
 
