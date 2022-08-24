dd20994cb8d5202a98119eb0d18c14b366dfd459
Mark Alston <malston@vmware.com>
2022-08-24 11:48:02 -0600

    Update persistent_disks table with hcx disk cids

--- bosh/bosh.go
+++ bosh/bosh.go
@@ -2,11 +2,12 @@ package bosh
 
 import (
 	"fmt"
+
 	"github.com/jmoiron/sqlx"
 )
 
 const (
-	UpdatePersistentDisk     = "UPDATE persistent_disks SET disk_cid=%s WHERE disk_cid=%s\n"
+	UpdatePersistentDisk     = "UPDATE persistent_disks SET disk_cid=$1 WHERE disk_cid=$2"
 	GetPersistentDiskMapping = `SELECT disk_cid, cid FROM persistent_disks INNER JOIN instances ON persistent_disks.instance_id=instances.id INNER JOIN deployments ON instances.deployment_id=deployments.id INNER JOIN vms ON persistent_disks.instance_id=vms.instance_id WHERE deployments.name=$1;`
 )
 
@@ -25,11 +26,32 @@ func (b BOSH) UpdatePersistentDiskCIDs(deployment string) error {
 		return err
 	}
 
-	for _, m := range diskMappings {
-		fmt.Printf(UpdatePersistentDisk, m.VmCID+"_3", m.DiskCID)
+	db := b.DB
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare(UpdatePersistentDisk)
+	if err != nil {
+		return err
 	}
 
-	return nil
+	for _, m := range diskMappings {
+		fmt.Printf("UPDATE persistent_disks SET disk_cid=%s WHERE disk_cid=%s\n", m.VmCID+"_3", m.DiskCID)
+		_, err = stmt.Exec(m.VmCID+"_3", m.DiskCID)
+		if err != nil {
+			_ = stmt.Close()
+			return err
+		}
+	}
+	err = stmt.Close()
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
 
 func (b BOSH) getPersistentDiskMappings(deployment string) ([]diskMappingsRow, error) {
