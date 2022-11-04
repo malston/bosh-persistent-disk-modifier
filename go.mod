de04feb1acece740d1cc97657c9f8663efa6fc6d
Mark Alston <malston@vmware.com>
2022-11-04 11:37:56 -0600

    Fetch disk name from vcenter

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/malston/bosh-persistent-disk-modifier
 go 1.16
 
 require (
+	github.com/dougm/pretty v0.0.0-20171025230240-2ee9d7453c02
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.6
+	github.com/vmware/govmomi v0.29.0
 	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
 )
---
+++ vc/vm.go
@@ -0,0 +1,33 @@
+package vc
+
+import (
+	"context"
+
+	"github.com/vmware/govmomi/find"
+	"github.com/vmware/govmomi/vim25"
+	"github.com/vmware/govmomi/vim25/types"
+)
+
+func GetPersistentDiskName(ctx context.Context, c *vim25.Client, vmName string) (error, string) {
+	finder := find.NewFinder(c)
+	vm, err := finder.VirtualMachine(ctx, vmName)
+	devices, err := vm.Device(ctx) // get the VM's virtual device list
+	if err != nil {
+		return err, ""
+	}
+
+	for i := range devices {
+		switch d := devices[i].(type) {
+		case *types.VirtualDisk:
+			switch d.Backing.(type) {
+			case *types.VirtualDiskFlatVer2BackingInfo:
+				disk := d.Backing.(*types.VirtualDiskFlatVer2BackingInfo)
+				if disk.DiskMode == "independent_persistent" {
+					return nil, disk.FileName
+				}
+			}
+		}
+	}
+
+	return nil, ""
+}
