3b50e4198827bc7ff2c5758814daab5d4e7f4d22
Mark Alston <malston@vmware.com>
2022-11-04 12:05:26 -0600

    Use default disk id for disk name when empty

--- bosh/repository.go
+++ bosh/repository.go
@@ -3,17 +3,18 @@ package bosh
 import (
 	"context"
 	"fmt"
-	"net/url"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/malston/bosh-persistent-disk-modifier/vc"
-	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/vim25"
 )
 
 const (
 	UpdatePersistentDisk     = "UPDATE persistent_disks SET disk_cid=$1 WHERE disk_cid=$2"
 	GetPersistentDiskMapping = `SELECT disk_cid, cid FROM persistent_disks INNER JOIN instances ON persistent_disks.instance_id=instances.id INNER JOIN deployments ON instances.deployment_id=deployments.id INNER JOIN vms ON persistent_disks.instance_id=vms.instance_id WHERE deployments.name=$1;`
 )
+
+const DefaultDiskID = "3"
 
 type Repository struct {
 	DB *sqlx.DB
@@ -24,12 +25,7 @@ type diskMappingsRow struct {
 	DiskCID string `db:"disk_cid"`
 }
 
-func (r Repository) UpdatePersistentDiskCIDs(deployment string, u *url.URL, insecure bool) error {
-	c, err := govmomi.NewClient(context.Background(), u, insecure)
-	if err != nil {
-		return fmt.Errorf("failed to create govmomi client, %w", err)
-	}
-
+func (r Repository) UpdatePersistentDiskCIDs(deployment string, c *vim25.Client) error {
 	diskMappings, err := r.getPersistentDiskMappings(deployment)
 	if err != nil {
 		return err
@@ -47,11 +43,15 @@ func (r Repository) UpdatePersistentDiskCIDs(deployment string, u *url.URL, inse
 	}
 
 	for _, m := range diskMappings {
-		err, diskName := vc.GetPersistentDiskName(context.Background(), c.Client, m.VmCID)
+		err, diskName := vc.GetPersistentDiskName(context.Background(), c, m.VmCID)
 		if err != nil {
 			_ = stmt.Close()
 			return err
 		}
+		if diskName == "" {
+			diskName = fmt.Sprintf("%s_%s", m.VmCID, DefaultDiskID)
+		}
+
 		fmt.Printf("UPDATE persistent_disks SET disk_cid=%s WHERE disk_cid=%s\n", diskName, m.DiskCID)
 		_, err = stmt.Exec(diskName, m.DiskCID)
 		if err != nil {
